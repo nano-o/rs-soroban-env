897c21caebf61f37ace38996ef478f99f31cf8df
Giuliano Losa <giuliano@losa.fr>
2023-03-01 20:42:57 -0800

    add allocator, panic handler, and fixes

--- soroban-env-verification/src/lib.rs
+++ soroban-env-verification/src/lib.rs
@@ -1,3 +1,16 @@
+#![no_std]
 mod verification_env;
 pub use verification_env::NoStdEnv;
 pub use soroban_env_common::*;
+use seahorn_stubs::error;
+use libc_alloc::LibcAlloc;
+use core::panic::PanicInfo;
+
+#[global_allocator]
+static ALLOCATOR: LibcAlloc = LibcAlloc;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    error()
+}
